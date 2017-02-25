560e4c284e8b93d0bd02a8b092727fdb6aa33153
Sergiy Dmytrenko <dmytrenko.serhii@gmail.com>
2017-02-25 21:03:15 +0200

    Shallow route release

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import Genre from './components/Genre'
 import Home from './components/Home'
 import NotFound from './components/NotFound'
 import List from './components/List'
+import Release from './components/Release'
 
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
@@ -16,7 +17,9 @@ render(
     <Route path='/' component={App}>
       <IndexRoute component={Home} />
       <Route path='admin' component={Admin} />
-      <Route path='genre/:genre' component={Genre} />
+      <Route path='genre/:genre' component={Genre}>
+        <Route path=':release' component={Release}/>
+      </Route>
       <Route path='list' component={List} />
     </Route>
     {/* для всех остальных роутов: показывай NotFound */}
