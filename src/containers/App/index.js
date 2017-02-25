9bb8ef71638e0e51da5807260b6d9973b5d70dbd
Sergiy Dmytrenko <dmytrenko.serhii@gmail.com>
2017-02-25 21:32:28 +0200

    Add active links

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
 
+import './styles.scss'
+
 export default class App extends Component {
 
   render() {
     return (
       <div className='container'>
-      <h1>App</h1>
-      <ul>
-        <li><Link to='/admin'>Админка</Link></li>
-        <li><Link to='/list'>Список жанров</Link></li>
-      </ul>
+        <ul className='nav nav-pills'>
+          <li><Link to='/admin' activeClassName='active'>Админка</Link></li>
+          <li><Link to='/list' activeClassName='active'>Список жанров</Link></li>
+        </ul>
         {/* добавили вывод потомков */}
         {this.props.children}
       </div>
