3311d391c452a3ceec4e066d30cd71a91a925627
Sergiy Dmytrenko <dmytrenko.serhii@gmail.com>
2017-02-26 13:31:06 +0200

    Division access

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -8,8 +8,9 @@ export default class App extends Component {
       <div className='container'>
         <ul className='nav nav-pills'>
           <li><NavLink onlyActiveOnIndex={true} to='/'>Главная</NavLink></li>
-          <li><NavLink onlyActiveOnIndex={true} to='/admin'>Админка</NavLink></li>
-          <li><NavLink onlyActiveOnIndex={true} to='/list'>Список жанров</NavLink></li>
+          <li><NavLink to='/admin'>Админка</NavLink></li>
+          <li><NavLink to='/list'>Список жанров</NavLink></li>
+          <li><NavLink to='/login'>Войти</NavLink></li>
         </ul>
         {/* добавили вывод потомков */}
         {this.props.children}
--- src/routes.js
+++ src/routes.js
@@ -8,16 +8,19 @@ import Home from './components/Home'
 import NotFound from './components/NotFound'
 import List from './components/List'
 import Release from './components/Release'
+import Login from './components/Login'
 
 export const routes = (
   <div>
   <Route path='/' component={App}>
     <IndexRoute component={Home} />
-    <Route path='/admin' component={Admin} />
+    {/* в качестве хука на onEnter - статический метод класса Admin */}
+    <Route path='/admin' component={Admin} onEnter={Admin.onEnter}/>
     <Route path='/genre/:genre' component={Genre}>
       <Route path='/genre/:genre/:release' component={Release}/>
     </Route>
     <Route path='/list' component={List} />
+    <Route path='/login' component={Login} />
   </Route>
   {/* для всех остальных роутов: показывай NotFound */}
   <Route path='*' component={NotFound} />
